33d0105ee6eda4dfd6cd8a1b177723af5d702794
handiko <handikogesang@gmail.com>
2018-11-13 00:33:05 +0700

    Adding test analog read

---
+++ test_analog_read/test_analog_read.ino
@@ -0,0 +1,23 @@
+#include<LiquidCrystal.h>
+
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+float freq = 124.31;
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  freq = analogRead(A0);
+  freq = freq + (freq/100);
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Baseband (Hz)");
+  lcd.setCursor(0,1);
+  lcd.print(freq);
+
+  delay(100);
+}
