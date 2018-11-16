197236d896475abb074033fe6c4a105af540d0d7
handiko <handikogesang@gmail.com>
2018-11-16 22:45:39 +0700

    First commit

--- Serial_GPS_RX/Serial_GPS_RX.ino
+++ Serial_GPS_RX/Serial_GPS_RX.ino
@@ -1,5 +1,10 @@
-//  Basically do nothing except to set pin-1 as an input 
-//  (to ensure hi impedance condition)
+/*  Basically do nothing except to set pin-1 as an input 
+    (to ensure hi impedance condition)
+
+    Instructions:
+    - Give proper +5V and GND to the GPS module
+    - Connect GPS TXD pin to the pin-1 of Arduino UNO
+*/
 
 void setup() {
   // put your setup code here, to run once:
