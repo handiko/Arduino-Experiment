49ba7786ceb5efda0cb52ab4dacf589f49aeee73
handiko <handikogesang@gmail.com>
2018-11-16 22:44:47 +0700

    First commit

---
+++ Serial_GPS_RX/Serial_GPS_RX.ino
@@ -0,0 +1,12 @@
+//  Basically do nothing except to set pin-1 as an input 
+//  (to ensure hi impedance condition)
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(1,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(100);
+}
