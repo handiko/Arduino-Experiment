7ed7f8511f82aa80fc4eeef842878b295ad3616a
handiko <handikogesang@gmail.com>
2018-11-18 05:07:02 +0700

    Adding Tone Gen

--- Tone_Gen/Tone_Gen.ino
+++ Tone_Gen/Tone_Gen.ino
@@ -1,10 +1,4 @@
-/*  Basically do nothing except to set pin-1 as an input 
-    (to ensure hi impedance condition)
-
-    Instructions:
-    - Give proper +5V and GND to the GPS module
-    - Connect GPS TXD pin to the pin-1 of Arduino UNO
-*/
+#include "pitches.h"
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,5 +7,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //tone(13,1000);
+  tone(13,NOTE_C4);
+  delay(1000);
+  noTone(13);
+  tone(13,NOTE_A3);
+  delay(1000);
+  noTone(13);
 }
