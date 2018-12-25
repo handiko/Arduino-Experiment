020814b30748a58d42de4e9d2d3626b381a4bfe1
handiko <handikogesang@gmail.com>
2018-12-26 00:54:36 +0700

    Recompile

--- APRS_string_test_UNO/APRS_string_test_UNO.ino
+++ APRS_string_test_UNO/APRS_string_test_UNO.ino
@@ -4,6 +4,12 @@
 
 #include <math.h>
 
+#define UNO     1
+#define DUE     2
+#define esp     3
+
+#define BOARD   UNO
+
 /*
  * 
  */
@@ -12,7 +18,19 @@ char stuff=0;
 unsigned short crc=0xffff;
 bool nada=0;
 
-const float baud_adj = 1.0;
+#ifdef BOARD
+  #if BOARD == 1
+    const float baud_adj = 0.98;
+    #define OUT_PIN 2
+  #elif BOARD == 2
+    const float baud_adj = 1.01;
+    #define OUT_PIN 3
+  #elif BOARD == 3
+    const float baud_adj = 1.000;
+    #define OUT_PIN 14
+  #endif
+#endif
+
 const float adj_1200 = 1.0 * baud_adj;
 const float adj_2400 = 1.0 * baud_adj;
 const unsigned int tc1200 = (unsigned int)(0.5 * adj_1200 * 1000000.0 / 1200.0);
@@ -22,12 +40,19 @@ const unsigned int tc2400 = (unsigned int)(0.5 * adj_2400 * 1000000.0 / 2400.0);
  * 
  */
 
-const unsigned char mycall[]={"YD1SDL-1"};
+const unsigned char mycall[]={"YD1SDL2"};
 const unsigned char dest[]={"APRS   "};
-const unsigned char digi[]={"WIDE2 "};
-const char digi_ssid=2;
+const unsigned char digi[]={"WIDE2 2"};
 
-unsigned char strings[]={" Hello World!"};
+const char strings[1000]={
+"b0NWtAXLKj0Sn8WRsakzQS8JN25zAAf3md5ILaYty6jvZHrq1QU1CWfC6tKOMY7cFCopla9sn0bn26zcd9qRHFWflqMcmMwx9ZDmzxrs4cfjiMox4R0pNCB0fm26gDVc\
+dMCZcVOnovLDWUlFHL0m2ULj3SVJonE4swIlemv2miVFJ3hjETh54cubpJhefhHtOGlwwtd64PigxsjzB3oXI6tJR3sCd84sheQis2DrnBZPd4pYdZvv6nx01hDeQNiU\
+YGilAHb7cdqlEIMwhHVaqIgn43JOwQzSMGOWvAbFdSxLyoUd8rYeyVWHxW3tyJS7wjWjsr1UV3RCkPBL4XhMpceV3z0zu6y9rQGWxBwVAbBliOo630lkdmwRkuMB0INN\
+cS4CjELYzsVQnEnX5OMCryDdbFEGwCpDEiFPETlP4EeqsYI6ACIRsM9A8buf1eecrwBKgkT3Ty0mHlOjc4ibBiJCJB5vTzvEbQdfgsLGubfPL1Y8Vb5PAzwCGVotWxUP\
+UPamGgBezXZ4JbOAbUfXGEM1ppuRtam8zk4ePExs1ccD4qumNt0pvfEWyCiIrVuLAK1TGoOG9rE0U0wCaLILlmLiTu1UtPMSTm1sZzEAdunENMmMrHH4bO5W3dL36Njo\
+q7fCVyFGiIurYBcmamYRWHFas3f6DCN7IpOiKo0PM1EIf7eeVegEB4lQZ5EVSXJ4HpGodk4h903bu4KIfm2VilJUUtjiy9lMqTXGliafDss5zBGpL8S7yh1z2NdgD8Tr\
+RGXR4EJ9gSiJTCBiGoSe1uzoeqPNV1pMM7ld7bKbTriOlBNyTCm7lx7cM8J5IsO4iegCSjG0OzwiQEhed7hvS2b78Quw9wDyVQijpxrWBkeFOqk6V656ISLomil0FrAF"
+};
 
 /*
  * 
@@ -49,20 +74,20 @@ void set_nada(bool in_nada)
 {
   if(nada)
   {
-    digitalWrite(2, HIGH);
+    digitalWrite(OUT_PIN, HIGH);
     delayMicroseconds(tc1200);
-    digitalWrite(2, LOW);
+    digitalWrite(OUT_PIN, LOW);
     delayMicroseconds(tc1200);
   }
   else
   {
-    digitalWrite(2, HIGH);
+    digitalWrite(OUT_PIN, HIGH);
     delayMicroseconds(tc2400);
-    digitalWrite(2, LOW);
+    digitalWrite(OUT_PIN, LOW);
     delayMicroseconds(tc2400);
-    digitalWrite(2, HIGH);
+    digitalWrite(OUT_PIN, HIGH);
     delayMicroseconds(tc2400);
-    digitalWrite(2, LOW);
+    digitalWrite(OUT_PIN, LOW);
     delayMicroseconds(tc2400);
   }
 }
@@ -124,7 +149,9 @@ void send_char(unsigned char in_byte)
 
 void send_ax25(void)
 {
-  for(int i=0;i<100;i++)
+  int temp;
+  
+  for(int i=0;i<50;i++)
     send_char(0x7e);
 
   crc = 0xffff;
@@ -137,13 +164,15 @@ void send_ax25(void)
 
   for(int i=0;i<6;i++)
     send_char(digi[i] << 1);
-  send_char((digi_ssid << 1) + 1);
+  send_char((digi[6] << 1) + 1);
 
   send_char(0x03);
   send_char(0xf0);
 
-  for(int i=0;i<sizeof(strings);i++)
-    send_char(strings[i]);
+  send_char(',');
+  temp = random(1, 600);
+  for(int i=0;i<200;i++)
+    send_char(strings[i + temp]);
   
   send_crc();
 
@@ -158,12 +187,11 @@ void send_ax25(void)
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(2, OUTPUT);
+  pinMode(OUT_PIN, OUTPUT);
 }
 
 void loop()
 {
   delay(2000);
   send_ax25();
-  nada ^= 1;
 }
