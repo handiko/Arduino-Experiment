fd017c3a180fd91aff05168ff8d0af8ee79c451d
handiko <handikogesang@gmail.com>
2019-01-03 20:51:36 +0700

    Recompile Dorji Test

--- Dorji-818V-Test/Dorji-818V-Test.ino
+++ Dorji-818V-Test/Dorji-818V-Test.ino
@@ -3,7 +3,10 @@
  */
 #include <SoftwareSerial.h>
 
-SoftwareSerial dorji(8,9);
+#define DRJ_TXD 10
+#define DRJ_RXD 11
+
+SoftwareSerial dorji(DRJ_RXD, DRJ_TXD);
 
 void dorji_init(SoftwareSerial &ser);
 void dorji_reset(SoftwareSerial &ser);
@@ -44,13 +47,45 @@ void dorji_readback(SoftwareSerial &ser)
 
 void setup()
 {
+  pinMode(DRJ_RXD, INPUT);
+  pinMode(DRJ_TXD, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  digitalWrite(7, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(5, LOW);
+  
   Serial.begin(115200);
   dorji.begin(9600);
 
-  dorji_reset(dorji);
-  delay(100);
+  Serial.println("Starting \n");
+
+  delay(250);
+  
+  dorji_init(dorji);
+  dorji_readback(dorji);
+
+  delay(1000);
+  
   dorji_setfreq(144.390, 144.390, dorji);
   dorji_readback(dorji);
+
+  for(;;)
+  {
+    delay(1500);
+    digitalWrite(7,HIGH);
+    for(int i=0;i<500;i++)
+    {
+      digitalWrite(12,HIGH);
+      delay(1);
+      digitalWrite(12,LOW);
+      delay(1);
+    }
+    digitalWrite(7, LOW);
+  }
 }
 
 void loop()
