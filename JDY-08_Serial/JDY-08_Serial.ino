0018b6f6543535e1f86f9de6838b4aa552b980bc
handiko <handikogesang@gmail.com>
2019-01-12 23:13:41 +0700

    Recompile

--- JDY-08_Serial/JDY-08_Serial.ino
+++ JDY-08_Serial/JDY-08_Serial.ino
@@ -2,28 +2,34 @@
  * 
  */
  
-#include <AltSoftSerial.h>
+#include <SoftwareSerial.h>
 
-AltSoftSerial BT(8,9);
+SoftwareSerial BT(8,9);
 
-void BT_sendcmd(AltSoftSerial &ser, String s);
-void BT_readback(AltSoftSerial &ser);
+void BT_sendcmd(SoftwareSerial &ser, String s);
+void BT_readback(SoftwareSerial &ser);
 
-void BT_initBT(AltSoftSerial &ser);
+void BT_initBT(SoftwareSerial &ser);
 
-void BT_renew(AltSoftSerial &ser);
-void BT_reset(AltSoftSerial &ser);
-void BT_setname(AltSoftSerial &ser, String s);
-void BT_setmaj(AltSoftSerial &ser, int n);
+void BT_renew(SoftwareSerial &ser);
+void BT_reset(SoftwareSerial &ser);
+void BT_setname(SoftwareSerial &ser, String s);
+void BT_setmaj(SoftwareSerial &ser, int n);
+void BT_setmin(SoftwareSerial &ser, int n);
+void BT_setibe0(SoftwareSerial &ser, String s);
+void BT_setibe1(SoftwareSerial &ser, String s);
+void BT_setibe2(SoftwareSerial &ser, String s);
+void BT_setibe3(SoftwareSerial &ser, String s);
+void BT_setIBeaUUID(SoftwareSerial &ser, String s0, String s1, String s2, String s3);
 
-void BT_sendcmd(AltSoftSerial &ser, String s)
+void BT_sendcmd(SoftwareSerial &ser, String s)
 {
   Serial.print("\r\n> sending command :");
   Serial.println(s);
   ser.print(s);
 }
 
-void BT_readback(AltSoftSerial &ser)
+void BT_readback(SoftwareSerial &ser)
 {
   char s;
   while(ser.available() > 0)
@@ -33,7 +39,7 @@ void BT_readback(AltSoftSerial &ser)
   }
 }
 
-void BT_initBT(AltSoftSerial &ser)
+void BT_initBT(SoftwareSerial &ser)
 {
   Serial.flush();
   Serial.print("\r\n> sending command : AT \r\n");
@@ -45,34 +51,127 @@ void BT_initBT(AltSoftSerial &ser)
   }
 
   BT_readback(ser);
+  BT_readback(ser);
+  delay(100);
+  BT_readback(ser);
 }
 
-void BT_reset(AltSoftSerial &ser)
+void BT_reset(SoftwareSerial &ser)
 {
   Serial.flush();
   Serial.print("\r\n> sending command : AT+RESET \r\n");
   ser.print("AT+RESET");
+  
+  BT_readback(ser);
+  BT_readback(ser);
+  delay(1000);
   BT_readback(ser);
 }
 
-void BT_setname(AltSoftSerial &ser, String s)
+void BT_setname(SoftwareSerial &ser, String s)
 {
   Serial.flush();
   Serial.print("\r\n> sending command : AT+NAME");
   Serial.println(s);
   ser.print("AT+NAME");
   ser.print(s);
+  
+  BT_readback(ser);
+  BT_readback(ser);
+  delay(100);
   BT_readback(ser);
 }
 
-void BT_setmaj(AltSoftSerial &ser, int n)
+void BT_setmaj(SoftwareSerial &ser, int n)
 {
   Serial.flush();
   Serial.print("\r\n> sending command : AT+MARJ0x");
   Serial.println(n, HEX);
-  //ser.print("AT+NAME");
-  //ser.print(s);
-  //BT_readback(ser);
+  ser.print("AT+MARJ0x");
+  ser.print(n, HEX);
+  
+  BT_readback(ser);
+  BT_readback(ser);
+  delay(100);
+  BT_readback(ser);
+}
+
+void BT_setmin(SoftwareSerial &ser, int n)
+{
+  Serial.flush();
+  Serial.print("\r\n> sending command : AT+MINO0x");
+  Serial.println(n, HEX);
+  ser.print("AT+MINO0x");
+  ser.print(n, HEX);
+  
+  BT_readback(ser);
+  BT_readback(ser);
+  delay(100);
+  BT_readback(ser);
+}
+
+void BT_setibe0(SoftwareSerial &ser, String s)
+{
+  Serial.flush();
+  Serial.print("\r\n> sending command : AT+IBE0");
+  Serial.println(s);
+  ser.print("AT+IBE0");
+  ser.print(s);
+  
+  BT_readback(ser);
+  BT_readback(ser);
+  delay(100);
+  BT_readback(ser);
+}
+
+void BT_setibe1(SoftwareSerial &ser, String s)
+{
+  Serial.flush();
+  Serial.print("\r\n> sending command : AT+IBE1");
+  Serial.println(s);
+  ser.print("AT+IBE1");
+  ser.print(s);
+  
+  BT_readback(ser);
+  BT_readback(ser);
+  delay(100);
+  BT_readback(ser);
+}
+
+void BT_setibe2(SoftwareSerial &ser, String s)
+{
+  Serial.flush();
+  Serial.print("\r\n> sending command : AT+IBE2");
+  Serial.println(s);
+  ser.print("AT+IBE2");
+  ser.print(s);
+  
+  BT_readback(ser);
+  BT_readback(ser);
+  delay(100);
+  BT_readback(ser);
+}
+
+void BT_setibe3(SoftwareSerial &ser, String s)
+{
+  Serial.flush();
+  Serial.print("\r\n> sending command : AT+IBE3");
+  Serial.println(s);
+  ser.print("AT+IBE3");
+  ser.print(s);
+  
+  BT_readback(ser);
+  BT_readback(ser);
+  delay(100);
+  BT_readback(ser);
+}
+
+void BT_setIBeaUUID(SoftwareSerial &ser, String s0, String s1, String s2, String s3)
+{
+  BT_setibe0(BT, s0);
+  BT_setibe1(BT, s1);
+  BT_setibe2(BT, s2);
+  BT_setibe3(BT, s3);
 }
  
 void setup() 
@@ -86,15 +185,22 @@ void setup()
   Serial.print("BTserial started at ");
   Serial.println(9600);
   
-  BT_initBT(BT);              delay(100);
+  BT_initBT(BT);
   
   BT_reset(BT);
-
-  delay(500);
   
-  BT_initBT(BT);              delay(100);
-  BT_setname(BT, "handiko");  delay(100);
-  BT_setmaj(BT, 12345);       delay(100);
+  BT_initBT(BT);
+
+  BT_setname(BT, "handiko");
+  BT_setmaj(BT, 12345);
+  BT_setmin(BT, 23456);
+  BT_setIBeaUUID (BT,
+                  "74278BDA",
+                  "12345678",
+                  "01AB23CD",
+                  "0A1B2C3D");
+                  
+  BT_reset(BT);
 }
  
 void loop()
