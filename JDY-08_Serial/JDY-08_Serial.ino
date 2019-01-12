b81f2d4243041dd0ba0ed52e2f157946bd389d12
handiko <handikogesang@gmail.com>
2019-01-12 22:46:25 +0700

    Recompile

--- JDY-08_Serial/JDY-08_Serial.ino
+++ JDY-08_Serial/JDY-08_Serial.ino
@@ -2,27 +2,28 @@
  * 
  */
  
-#include <SoftwareSerial.h>
+#include <AltSoftSerial.h>
 
-SoftwareSerial BT(8,9);
+AltSoftSerial BT(8,9);
 
-void BT_sendcmd(SoftwareSerial &ser, String s);
-char BT_readback(SoftwareSerial &ser);
+void BT_sendcmd(AltSoftSerial &ser, String s);
+void BT_readback(AltSoftSerial &ser);
 
-void BT_initBT(SoftwareSerial &ser);
+void BT_initBT(AltSoftSerial &ser);
 
-void BT_renew(SoftwareSerial &ser);
-void BT_reset(SoftwareSerial &ser);
-void BT_setname(SoftwareSerial &ser, String s);
+void BT_renew(AltSoftSerial &ser);
+void BT_reset(AltSoftSerial &ser);
+void BT_setname(AltSoftSerial &ser, String s);
+void BT_setmaj(AltSoftSerial &ser, int n);
 
-void BT_sendcmd(SoftwareSerial &ser, String s)
+void BT_sendcmd(AltSoftSerial &ser, String s)
 {
   Serial.print("\r\n> sending command :");
   Serial.println(s);
   ser.print(s);
 }
 
-char BT_readback(SoftwareSerial &ser)
+void BT_readback(AltSoftSerial &ser)
 {
   char s;
   while(ser.available() > 0)
@@ -30,32 +31,48 @@ char BT_readback(SoftwareSerial &ser)
     s = ser.read();
     Serial.print(s);
   }
-  return s;
 }
 
-void BT_initBT(SoftwareSerial &ser)
+void BT_initBT(AltSoftSerial &ser)
 {
+  Serial.flush();
+  Serial.print("\r\n> sending command : AT \r\n");
+  
   for(char i=0;i<5;i++)
   {
     ser.print("AT");
     delay(100);
   }
+
+  BT_readback(ser);
 }
 
-void BT_reset(SoftwareSerial &ser)
+void BT_reset(AltSoftSerial &ser)
 {
-  Serial.print("\r\n> sending command : AT+RESET");
+  Serial.flush();
+  Serial.print("\r\n> sending command : AT+RESET \r\n");
   ser.print("AT+RESET");
   BT_readback(ser);
 }
 
-void BT_setname(SoftwareSerial &ser, String s)
+void BT_setname(AltSoftSerial &ser, String s)
 {
+  Serial.flush();
   Serial.print("\r\n> sending command : AT+NAME");
   Serial.println(s);
   ser.print("AT+NAME");
   ser.print(s);
   BT_readback(ser);
+}
+
+void BT_setmaj(AltSoftSerial &ser, int n)
+{
+  Serial.flush();
+  Serial.print("\r\n> sending command : AT+MARJ0x");
+  Serial.println(n, HEX);
+  //ser.print("AT+NAME");
+  //ser.print(s);
+  //BT_readback(ser);
 }
  
 void setup() 
@@ -69,11 +86,15 @@ void setup()
   Serial.print("BTserial started at ");
   Serial.println(9600);
   
-  BT_initBT(BT);
-  BT_readback(BT);
-
+  BT_initBT(BT);              delay(100);
+  
   BT_reset(BT);
-  BT_setname(BT, "handiko");
+
+  delay(500);
+  
+  BT_initBT(BT);              delay(100);
+  BT_setname(BT, "handiko");  delay(100);
+  BT_setmaj(BT, 12345);       delay(100);
 }
  
 void loop()
