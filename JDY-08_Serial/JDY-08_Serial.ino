3131a1916fc267b4dd756f57c29ffbbdf2efb3f1
handiko <handikogesang@gmail.com>
2019-01-12 21:33:11 +0700

    Recompile

--- JDY-08_Serial/JDY-08_Serial.ino
+++ JDY-08_Serial/JDY-08_Serial.ino
@@ -1,24 +1,12 @@
-//  SerialIn_SerialOut_HM-10_01
-//
-//  Uses hardware serial to talk to the host computer and AltSoftSerial for communication with the bluetooth module
-//
-//  What ever is entered in the serial monitor is sent to the connected device
-//  Anything received from the connected device is copied to the serial monitor
-//  Does not send line endings to the HM-10
-//
-//  Pins
-//  BT VCC to Arduino 5V out. 
-//  BT GND to GND
-//  Arduino D8 (SS RX) - BT TX no need voltage divider 
-//  Arduino D9 (SS TX) - BT RX through a voltage divider (5v to 3.3v)
-//
+/*
+ * 
+ */
  
 #include <AltSoftSerial.h>
 
-AltSoftSerial BTserial; 
- 
-char c=' ';
-boolean NL = true;
+AltSoftSerial btSerial;
+char c = ' ';
+bool NL = true;
  
 void setup() 
 {
@@ -27,16 +15,16 @@ void setup()
     Serial.print("Uploaded: ");   Serial.println(__DATE__);
     Serial.println(" ");
  
-    BTserial.begin(9600);  
+    btSerial.begin(9600);  
     Serial.print("BTserial started at ");
     Serial.println(9600);
 }
  
 void loop()
 {
-    if (BTserial.available())
+    if (btSerial.available())
     {
-        c = BTserial.read();
+        c = btSerial.read();
         Serial.write(c);
     }
  
@@ -45,13 +33,21 @@ void loop()
     {
         c = Serial.read();
  
-        if ((c!=10) & (c!=13) ) 
+        if ((c!=10) & (c!=13)) 
         {  
-             BTserial.write(c);
+             btSerial.write(c);
         }
  
-        if (NL) { Serial.print("\r\n>");  NL = false; }
+        if (NL)
+        {
+          Serial.print("\r\n>");  
+          NL = false; 
+        }
+        
         Serial.write(c);
-        if (c==10) { NL = true; }
+        if (c==10) 
+        { 
+          NL = true; 
+        }
     }
 }
