dc0772b45ca264467583ff64e4b1230b4384f301
handiko <handikogesang@gmail.com>
2019-01-22 06:38:27 +0700

    Adding GPS Struct Test

--- GPS_Struct/GPS_Struct.ino
+++ GPS_Struct/GPS_Struct.ino
@@ -26,6 +26,9 @@ char* parse_rmc_lat(char gps_str[]);
 char parse_rmc_NS(char gps_str[]);
 char* parse_rmc_lon(char gps_str[]);
 char parse_rmc_EW(char gps_str[]);
+char* parse_rmc_spd(char gps_str[]);
+char* parse_rmc_cse(char gps_str[]);
+char* parse_rmc_date(char gps_str[]);
 
 char* parse_rmc_time(char gps_str[])
 {
@@ -149,7 +152,8 @@ char parse_rmc_NS(char gps_str[])
     c++;
   }
 
-  ns = gps_str[c];
+  if(gps_str[c]!=',')
+    ns = gps_str[c];
 
   sprintf(buff, " N/S: %c", ns);
   Serial.println(buff);
@@ -216,12 +220,136 @@ char parse_rmc_EW(char gps_str[])
     c++;
   }
 
-  ew = gps_str[c];
+  if(gps_str[c]!=',')
+    ew = gps_str[c];
 
   sprintf(buff, " E/W: %c", ew);
   Serial.println(buff);
 
   return ew;
+}
+
+char* parse_rmc_spd(char gps_str[])
+{
+  char spd[10];
+  char *result;
+  int c=0;
+  int i=0;
+
+  char buff[50];
+
+  for(int j=0;j<10;j++)
+  {
+    spd[j] = 0;
+  }
+
+  for(i=0;i<7;i++)
+  {
+    while(gps_str[c] != ',')
+    {
+      c++;
+    }
+
+    c++;
+  }
+
+  i=0;
+
+  while(gps_str[c] != ',')
+  {
+    spd[i] = gps_str[c];
+    c++;
+    i++;
+  }
+
+  result = spd;
+
+  sprintf(buff, " Speed (knots): %s", spd);
+  Serial.println(buff);
+  
+  return result;
+}
+
+char* parse_rmc_cse(char gps_str[])
+{
+  char cse[10];
+  char *result;
+  int c=0;
+  int i=0;
+
+  char buff[50];
+
+  for(int j=0;j<10;j++)
+  {
+    cse[j] = 0;
+  }
+
+  for(i=0;i<8;i++)
+  {
+    while(gps_str[c] != ',')
+    {
+      c++;
+    }
+
+    c++;
+  }
+
+  i=0;
+
+  while(gps_str[c] != ',')
+  {
+    cse[i] = gps_str[c];
+    c++;
+    i++;
+  }
+
+  result = cse;
+
+  sprintf(buff, " Course (degree): %s", cse);
+  Serial.println(buff);
+  
+  return result;
+}
+
+char* parse_rmc_date(char gps_str[])
+{
+  char gps_date[10];
+  char *result;
+  int c=0;
+  int i=0;
+
+  char buff[50];
+
+  for(int j=0;j<10;j++)
+  {
+    gps_date[j] = 0;
+  }
+
+  for(i=0;i<9;i++)
+  {
+    while(gps_str[c] != ',')
+    {
+      c++;
+    }
+
+    c++;
+  }
+
+  i=0;
+
+  while(gps_str[c] != ',')
+  {
+    gps_date[i] = gps_str[c];
+    c++;
+    i++;
+  }
+
+  result = gps_date;
+
+  sprintf(buff, " Date: %s", gps_date);
+  Serial.println(buff);
+  
+  return result;
 }
 
 int gps_parse(SoftwareSerial &ser)
@@ -277,6 +405,9 @@ int gps_parse(SoftwareSerial &ser)
   parse_rmc_NS(rmc);
   parse_rmc_lon(rmc);
   parse_rmc_EW(rmc);
+  parse_rmc_spd(rmc);
+  parse_rmc_cse(rmc);
+  parse_rmc_date(rmc);
 
   Serial.println(' ');
   
