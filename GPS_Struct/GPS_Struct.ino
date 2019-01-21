e982ad24c8b5a42170da48478a3e34d4ff4b26af
handiko <handikogesang@gmail.com>
2019-01-22 04:57:42 +0700

    Adding GPS Struct Test

--- GPS_Struct/GPS_Struct.ino
+++ GPS_Struct/GPS_Struct.ino
@@ -18,6 +18,102 @@
 
 SoftwareSerial gps = SoftwareSerial(8, 9);
 
+struct GPS 
+{
+  char time_fix[10];
+  char valid;
+  char lati[12];
+  char NS;
+  char lon[12];
+  char EW;
+  char spd[10];
+  char cse[10];
+  char date_fix[10];
+  char magnetic[10];
+  char mag_EW;
+  char check[5];
+};
+
+int parse_rmc_time(char gps_str[])
+{
+  int c=0;
+  int i;
+
+  for(;c<150;c++)
+  {
+    if(gps_str[c] == ',')
+      break;
+  }
+  
+  c++;
+
+  Serial.print(" nilai c: ");
+  Serial.println(c);
+
+  while(gps_str[c] != ',')
+  {
+    GPS.time_fix[i] = gps_str[c];
+    c++;
+    i++;
+  }
+
+  Serial.print(" nilai i: ");
+  Serial.println(i);
+  
+  return i;
+}
+
+int gps_parse(SoftwareSerial &ser)
+{
+  char rmc[150];
+  char temp;
+  int c=0;
+
+  for(int i=0;i<150;i++)
+    rmc[i]='\0';
+
+  do
+  {
+    if(ser.available()>0)
+      temp = ser.read(); 
+  }
+  while(temp!='$');
+
+  rmc[c] = temp;
+  c++;
+
+  do
+  {
+    if(ser.available()>0)
+    {
+      temp = ser.read();
+      rmc[c] = temp;
+      c++;
+    }
+
+    if(c==6)
+    {
+      if(rmc[5]!='C')
+      {
+        return 0;
+
+        goto esc;
+      }
+    }
+  }
+  while((temp!=10)&&(temp!=13));
+
+  c--;
+
+  Serial.println(rmc);
+  parse_rmc_time(rmc);
+  
+  return c;
+
+  esc:
+  ;
+}
+
 void setup()
 {
   Serial.begin(115200);
@@ -28,9 +124,11 @@ void setup()
   Serial.println(" ");
   
   Serial.println("GPS Struct Test - Started ! \n");
+
+  gps.begin(9600);
 }
 
 void loop()
 {
-  
+  gps_parse(gps);
 }
