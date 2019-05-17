e621435677d13f0f7c5a2d893501b8e3adcd466c
handiko <handikogesang@gmail.com>
2019-05-17 09:37:11 +0700

    new experiment

--- WemosD1_MQTT_RF_Jammer/WemosD1_MQTT_RF_Jammer.ino
+++ WemosD1_MQTT_RF_Jammer/WemosD1_MQTT_RF_Jammer.ino
@@ -1,19 +1,9 @@
-/*
- *  Copyright (C) 2018 - Handiko Gesang - www.github.com/handiko
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <PubSubClient.h>
+
 #include <AD9851.h>
 
 #define RST   12
@@ -23,8 +13,104 @@
 
 DDS dds;
 
+ESP8266WiFiMulti wifiMulti;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const char* brokerUser = "handikogesang@gmail.com";
+const char* brokerPass = "45f02257";
+const char* broker = "mqtt.dioty.co";
+
+const char* outTopicStatus  = "/handikogesang@gmail.com/out/stat";
+const char* outTopicMaxFreq = "/handikogesang@gmail.com/out/maxFreq";
+const char* outTopicMinFreq = "/handikogesang@gmail.com/out/minFreq";
+
+const char* inTopicEnable   = "/handikogesang@gmail.com/in/en";
+const char* inTopicMaxFreq  = "/handikogesang@gmail.com/in/maxFreq";
+const char* inTopicMinFreq  = "/handikogesang@gmail.com/in/minFreq";
+
+long currentTime, lastTime;
+bool enDDS = false;
+
 unsigned long min_freq = 34800000UL;
 unsigned long max_freq = 35200000UL;
+
+void setupWiFi()
+{
+  delay(100);
+  
+  wifiMulti.addAP("YC1SDL", "Metallica");
+  wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
+  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+
+  Serial.println("Connecting ...");
+
+  digitalWrite(LED_BUILTIN, LOW);
+
+  while (wifiMulti.run() != WL_CONNECTED) 
+  {
+    delay(250);
+    Serial.print('.');
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  Serial.println('\n');
+  Serial.print("Connected to:\t");
+  Serial.println(WiFi.SSID());
+  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect()
+{
+  while(!client.connected())
+  {
+    Serial.print("\nConnecting to ");
+    Serial.println(broker);
+
+    if(client.connect("WemosD1", brokerUser, brokerPass))
+    {
+      Serial.print("\nConnected to ");
+      Serial.println(broker);
+      
+      client.subscribe(inTopicEnable);
+      client.subscribe(inTopicMaxFreq);
+      client.subscribe(inTopicMinFreq);
+    }
+    else
+    {
+      Serial.println("Connecting");
+      
+      delay(2500);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int len)
+{
+  Serial.print("Received messages: ");
+  Serial.println(topic);
+  
+  for(unsigned int i=0; i<len; i++)
+  {
+    Serial.print((char) payload[i]);
+
+    if(len == 1 && payload[0] == '1')
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+      enDDS = true;
+    }
+    else if(len == 1 && payload[0] == '0')
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      enDDS = false;
+    }
+  }
+  
+  Serial.println();
+}
 
 void setup()
 {
@@ -35,14 +121,49 @@ void setup()
   Serial.print("Uploaded: ");   Serial.println(__DATE__);
   Serial.println(" ");
   
-  Serial.println("Test - Started ! \n");
+  pinMode(LED_BUILTIN, OUTPUT);
 
   dds = dds_init(RST, DATA, FQ, CLK);
   dds_reset(dds);
   writeFreq(dds, min_freq);
+  
+  setupWiFi();
+  
+  client.setServer(broker, 1883);
+  client.setCallback(callback);
 }
 
 void loop()
 {
-  writeFreq(dds, random(min_freq, max_freq));
+  char messageEn[50];
+  
+  if(!client.connected())
+  {
+    reconnect();
+  }
+
+  client.loop();
+
+  currentTime = millis();
+  
+  if(currentTime - lastTime > 1500)
+  {
+    if(enDDS)
+    {
+      writeFreq(dds, random(min_freq, max_freq));
+      
+      snprintf(messageEn, 75, "Enabled");
+    }
+    else
+    {
+      snprintf(messageEn, 50, "Disabled");
+    }
+
+    Serial.print("Sending messages:\t");
+    Serial.println(messageEn);
+    
+    client.publish(outTopicStatus, messageEn);
+    
+    lastTime = millis();
+  }
 }
